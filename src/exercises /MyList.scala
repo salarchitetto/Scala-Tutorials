a4b6d60f4e521739f57e587635b7399a078bc462
salarchitetto <sal.architetto862@gmail.com>
2019-12-24 10:20:48 -0500

    attempting pull fix

--- src/exercises /MyList.scala
+++ src/exercises /MyList.scala
@@ -1,24 +1,5 @@
 package exercises
 
-import scala.collection.JavaConverters._
-
-abstract class MyList {
-  /*
-    head = first element of this list
-    tail = remainder of the list (Pointer)
-    isEmpty = boolean - is this list empty
-    add(int) = new list with this element added
-    toString = override, returns a string representation of the list
-   */
-
-  def head: Int
-  def tail: MyList
-  def isEmpty: Boolean
-  def add(element: Int): MyList
-  def printElements: String
-  override def toString: String = "[" + printElements + "]"
-}
-
 object Empty extends MyList {
   def head: Int = throw new NoSuchElementException // returning nothing
   def tail: MyList = throw new NoSuchElementException
